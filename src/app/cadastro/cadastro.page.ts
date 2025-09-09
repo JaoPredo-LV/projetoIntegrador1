c02bd9a3364ca4461fb7068669155852a1319122
ANA JULIA MAFIOLETE MEDEIROS <ana.medeiros4@alunos.sc.senac.br>
2025-09-09 16:42:16 -0300

    alterado css

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonText, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonText, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { home, personCircleOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonLabel, IonItem, IonList, IonText, IonButton, RouterLink, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonLabel, IonItem, IonList, IonText, IonButton, RouterLink, CommonModule, FormsModule, IonIcon]
 })
 export class CadastroPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({home, personCircleOutline})
+   }
 
   ngOnInit() {
   }
