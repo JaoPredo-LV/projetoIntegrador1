4185670d174f347b5fb11a64d78cb78a12c5bcc4
anaju1719 <mafioletemedeirosanajulia@gmail.com>
2025-09-17 09:19:01 -0300

    arrumando a pag

--- src/app/questionario/questionario.page.ts
+++ src/app/questionario/questionario.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonIcon,} from '@ionic/angular/standalone';
 import { chevronForwardOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons'
+import { IonCheckbox } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-questionario',
   templateUrl: './questionario.page.html',
   styleUrls: ['./questionario.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonIcon, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonButton, IonIcon, CommonModule, FormsModule, IonCheckbox]
 })
 export class QuestionarioPage implements OnInit {
   constructor() { 
