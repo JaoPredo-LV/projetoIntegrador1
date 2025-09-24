323544d89a77151a3cb61a20d0470463acdfbcc6
Pedrof <pedrofigueiredozanette@gmail.com>
2025-09-24 11:37:52 -0300

    fiz os negocio que pediro ai

--- src/app/niveleliptco/niveleliptco.page.ts
+++ src/app/niveleliptco/niveleliptco.page.ts
@@ -15,12 +15,16 @@ export class NiveleliptcoPage implements OnInit, OnDestroy {
   tempoDoTreino: number = 0;
   progressoPorcentagem: number = 0;
   private intervalo: any;
-  alturasFinais: number[] = [60, 80, 40, 70, 90, 60, 45];
-  barrasAtivas: boolean[] = [false, false, false, false, false, false, false];
+  alturasFinais: number[] = [60, 80, 40, 70, 90, 60, 45, 80, 60];
+  barrasAtivas: boolean[] = [false, false, false, false, false, false, false, false, false];
   barraAtualIndex: number = 0;
   alturasAtuais: number[] = [0, 0, 0, 0, 0, 0, 0];
   velocidadePreenchimento: number = 1; // Você pode ajustar este valor
   treinoConcluido: boolean = false;
+  rpms: string[] = ['50-60', '60-70', '70-80', '60-70', '35-40', '70-80', '80-90','60-70','50-60'];
+  cargas: string[] = ['0-3', '0-3', '0-3', '0-3', '0-3', '4-7','0-3', '0-3'];
+  rpmAtual: string = this.rpms[0];
+  cargaAtual: string = this.cargas[1];
 
   constructor() { }
 
@@ -51,6 +55,16 @@ export class NiveleliptcoPage implements OnInit, OnDestroy {
                 this.alturasAtuais[this.barraAtualIndex] = this.alturasFinais[this.barraAtualIndex];
                 this.barraAtualIndex++;
             }
+
+            if (this.barraAtualIndex < this.rpms.length) {
+          this.rpmAtual = this.rpms[this.barraAtualIndex];
+          if (this.barraAtualIndex + 1 < this.cargas.length) {
+            this.cargaAtual = this.cargas[this.barraAtualIndex + 1];
+          } else {
+            this.cargaAtual = '';
+          }
+        }
+        
         } else {
           
           this.treinoConcluido = true;
