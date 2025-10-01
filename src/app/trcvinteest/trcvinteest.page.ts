5f615c2bd47821395fb45081e0d0fee447a0590b
anaju1719 <mafioletemedeirosanajulia@gmail.com>
2025-10-01 10:19:22 -0300

    feitos hits eliptico

--- src/app/trcvinteest/trcvinteest.page.ts
+++ src/app/trcvinteest/trcvinteest.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { IonContent, IonBackButton, IonButtons, IonButton, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-trcvinteest',
   templateUrl: './trcvinteest.page.html',
   styleUrls: ['./trcvinteest.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, RouterModule, IonBackButton, IonButtons, IonButton, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
+
 export class TrcvinteestPage implements OnInit {
 
   constructor() { }
